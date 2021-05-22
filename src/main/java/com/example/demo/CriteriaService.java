59cca7150bd849b875dc0f9339462e14851a2c7a
Maxim Zykov <maxim_zykov@inbox.ru>
2021-05-22 05:33:24 +0300

    filters v1

--- src/main/java/com/example/demo/CriteriaService.java
+++ src/main/java/com/example/demo/CriteriaService.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.google.common.collect.Lists;
+import com.google.common.base.Functions;
 import com.example.demo.Category.Category;
 import com.example.demo.Category.CategoryRepository;
 import com.example.demo.Product.Product;
@@ -11,12 +13,16 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.exact;
 
@@ -78,5 +84,35 @@ public class CriteriaService {
         Example<Category> example = Example.of(category, matcher);
         System.out.println(example);
         return categoryRepository.findAll(example);
+    }
+
+    public List<Product> takeProductList(Category category, int[] rating, int quantity,
+                                          String[] manufactures_list, String filterName,
+                                          Integer minPrice, Integer maxPrice, List<String> manufactures_set) {
+        int MinPrice = 0;
+        int MaxPrice = 1000000;
+        if (minPrice !=null){
+            MinPrice = minPrice;
+        }
+        if (maxPrice !=null){
+            MaxPrice = maxPrice;
+        }
+        int minQuantity = 0;
+        int maxQuantity = 1000000;
+        if (quantity == 2) {
+            minQuantity = 1;
+        }
+        if (quantity == 3){
+            maxQuantity = 0;
+        }
+        List<Integer> Rating  = Arrays.asList(0, 1, 2, 3, 4, 5);
+        if (rating!=null){
+            Rating = IntStream.of(rating).boxed().collect(Collectors.toList());
+        }
+        if (manufactures_list!=null){
+            manufactures_set = Arrays.asList(manufactures_list);
+        }
+        return productRepository.findByNameContainingIgnoreCaseAndAndCategoryIdAndPriceBetweenAndQuantityBetweenAndManufacturerInAndRatingIn(
+                filterName, category.getId(), MinPrice, MaxPrice, minQuantity, maxQuantity, manufactures_set, Rating);
     }
 }
